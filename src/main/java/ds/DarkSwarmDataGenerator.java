06e4b44984c55874d354f1d5caece54258550ea1
Tima-01 <gmod1600@mail.ru>
2026-04-05 22:26:34 +0600

    Initial commit

---
+++ src/main/java/ds/DarkSwarmDataGenerator.java
@@ -0,0 +1,11 @@
+package ds;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DarkSwarmDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/ds/entity/ModEntities.java
@@ -0,0 +1,4 @@
+package ds.entity;
+
+public class ModEntities {
+}
